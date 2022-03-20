320fe4709a8ed6d5a7993a648be6f0a548a05671
unknown <ap.venkatesan@gmail.com>
2022-03-20 14:37:08 +0530

    My First Commit of all modules

---
+++ src/main/java/com/venkat/HelloWorld/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.venkat.HelloWorld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+    @GetMapping("/{name}")
+    public String sayHello(@PathVariable(value = "name", required = false) String name) {
+        return "Hi " + name;
+    }
+}
